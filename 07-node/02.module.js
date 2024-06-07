3da718e0e95f881195ed6b085c846761bd399770
anemoa <anemoia.yves@gmail.com>
2024-06-07 16:35:03 +0900

    study: module 01

---
+++ 07-node/02.module.js
@@ -0,0 +1,7 @@
+// math2 모듈 불러오기
+const math2 = require('./02.math');
+// console.log(math2);
+
+console.log(math2.PI);
+console.log(math2.E);
+console.log(math2.add(7, 7));
---
+++ 07-node/03.index.js
@@ -0,0 +1,4 @@
+const {add, PI, E} = require('./02.math');
+console.log(PI);
+console.log(E);
+console.log(add(8, 5));
