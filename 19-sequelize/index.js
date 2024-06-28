b1878cb81f19580328cf0809bd97c344b1b419f4
anemoa <anemoia.yves@gmail.com>
2024-06-28 17:47:44 +0900

    study: add api about player width sequelize

--- 19-sequelize/index.js
+++ 19-sequelize/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 const router = require('./routes/index');
+const playerRouter = require('./routes/player');
 const { sequelize } = require('./models');
 
 app.set('view engine', 'ejs');
@@ -10,11 +11,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/', router);
+app.use('/players', playerRouter);
 
 sequelize
     // force: true; 서버 실행때 마다 테이블을 재 생성
     // force: false; 서버 실행시 테이블이 없으면 생성
-    .sync({ force: true })
+    .sync({ force: false })
     .then( () => {
         app.listen(PORT, () => {
             console.log(`Database 연결 성공!`);
