bcb02e58e76639d5f22ffe2d3a799ae0aff4e1f7
anemoa <anemoia.yves@gmail.com>
2024-08-07 19:02:42 +0900

    study: ref & useRef

---
+++ 28-ref-lifecycle/src/App.js
@@ -0,0 +1,19 @@
+import RefSample4 from "./RefSample4";
+import { RefSample1 } from "./RefSample1";
+import { RefSample2 } from "./RefSample2";
+import RefSample3 from "./RefSample3";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <RefSample1 /> */}
+      {/* <RefSample2 /> */}
+      {/* <RefSample3 /> */}
+      <RefSample4 />
+    </div>
+  );
+}
+
+export default App;
