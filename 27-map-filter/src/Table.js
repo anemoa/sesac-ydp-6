21392906465789975e563dc38a84694941d7affa
anemoa <anemoia.yves@gmail.com>
2024-08-08 15:15:47 +0900

    study: 심화과제 미완성

--- 27-map-filter/src/Table.js
+++ 27-map-filter/src/Table.js
@@ -1,14 +1,17 @@
 import React,{useState} from 'react'
+import { PracTable } from './PracTable';
 
 export const Table = () => {
 
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([]); // 글 작성시 내용을 배열
+  const [user, setUser] = useState(''); // input에서 작성자 감지
+  const [title, setTitle] = useState(''); // input에서 제목 감지
+  const [typeVal, setTypeVal] = useState('user'); // select 에서 value 감지
+  const [search, setSearch] = useState(''); // 검색창 상태 감지
+  const [searchRes, setSearchRes] = useState([]); // 검색한 결과의 배열
 
-  const [user, setUser] = useState('');
-  const [title, setTitle] = useState('');
 
-
-  const addTxt = () => {
+  const addTxt = () => { // 작성한 글을 출력하게 해주는 함수
     const newTxt = list.concat({
       id: list.length + 1,
       user: user,
@@ -16,6 +19,48 @@ export const Table = () => {
     });
     console.log(newTxt);
     setList(newTxt);  
+  }
+
+  const typeChange = (e) => { // select 에서 선택한 value의 상태를 확인
+    setTypeVal(e.target.value);
+
+  }
+
+
+  const searchResult = (ele) => {
+    console.log('searchResult >?>', ele);
+    const {id, user, title} = ele;
+    console.log(id, user, title);
+    const newList2 = searchRes.concat({
+      id: id,
+      user: user,
+      title: title
+    });
+
+    setSearchRes(newList2);
+    console.log('이건 결과야!! >> ', searchRes);
+  }
+
+  const searchBtn = (id) => {
+    console.log('typeVal >>> ', typeVal);
+    // console.log(e);
+    if(typeVal == 'user'){
+      const newList = list.filter((ele) => console.log(ele.user)
+      );
+  
+    }else if(typeVal == 'title'){
+      const newList = list.filter((ele) => {
+        if(ele.title.includes(search)){
+          return searchResult(ele);
+        }else{
+          return;
+        }
+      });
+    }
+  }
+
+  const searchAll = () => {
+
   }
 
   return (
@@ -34,33 +79,26 @@ export const Table = () => {
 
 
       <div>
-        <select name="" id="">
+        <select onChange={typeChange}>
           <option value="user">작성자</option>
           <option value="title">제목</option>
         </select>
 
-        <input type="text" />
-        <button type='submit'>검색</button>
+        <input type="text" value={search} onChange={(e) => {
+          setSearch(e.target.value);
+        }}/>
+        <button type='submit' onClick={searchBtn}>검색</button>
+        <button type='button' onClick={searchAll}>전체</button>
 
       </div>
 
-      <table border='1'>
-          <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-          </tr>
+      <PracTable list={list} /> 
+      {/* 테이블은 컴포넌트로 따로 만들었으며, list를 props로 보내줍니다. */}
+      <hr />
 
-          {list.map((ele) => {
-            return (
-              <tr >
-                <td>{ele.id}</td>
-                <td>{ele.title}</td>
-                <td>{ele.user}</td>
-              </tr>
-            );
-          })}
-      </table>
+      {/* {search.length > 0 ? <PracTable /> : <div>검색없어요</div>} */}
+
+
     </div>
   )
 }
