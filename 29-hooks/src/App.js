7fc0cee2cf4cbd20a33f721ef320aad7adab6518
anemoa <anemoia.yves@gmail.com>
2024-08-09 18:29:55 +0900

    study: useReducer

--- 29-hooks/src/App.js
+++ 29-hooks/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { UseCallBackEx } from './Components/UseCallBackEx';
 import { UseCallbackEx2 } from './Components/UseCallbackEx2';
 import UseMemoEx from './Components/UseMemoEx';
+import { UseReducerEx } from './Components/UseReducerEx';
 
 function App() {
   return (
     <div className="App">
       {/* <UseMemoEx /> */}
-      <UseCallBackEx />
+      {/* <UseCallBackEx /> */}
       <hr />
-      <UseCallbackEx2 postId={9}/>
+      {/* <UseCallbackEx2 postId={9}/> */}
+      <UseReducerEx />
     </div>
   );
 }
