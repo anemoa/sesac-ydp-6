b0fef5638cdf696f10bcaaf00b71cf743baa50a0
anemoa <anemoia.yves@gmail.com>
2024-08-09 17:51:14 +0900

    study: useCallback

--- 29-hooks/src/App.js
+++ 29-hooks/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import { UseCallBackEx } from './Components/UseCallBackEx';
+import { UseCallbackEx2 } from './Components/UseCallbackEx2';
 import UseMemoEx from './Components/UseMemoEx';
 
 function App() {
   return (
     <div className="App">
-      <UseMemoEx />
+      {/* <UseMemoEx /> */}
+      <UseCallBackEx />
+      <hr />
+      <UseCallbackEx2 postId={9}/>
     </div>
   );
 }
