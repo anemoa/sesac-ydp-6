0c145270b8d782005f27f4fc7feefbcf9f730e26
anemoa <anemoia.yves@gmail.com>
2024-08-12 20:53:30 +0900

    study: scss 과제 01 & 02

--- 30-styles/src/App.js
+++ 30-styles/src/App.js
@@ -2,20 +2,25 @@ import './styles/App.css';
 import {CssModuleComponents} from './CssModuleComponents';
 import { SassComponents } from './SassComponents';
 import { StyledComponent } from './StyledComponent';
+import { Practice01 } from './Practice01';
+import { Practice02 } from './Practice02';
 
 function App() {
 
   return (
     <div className="App">
-      <h2>Sen</h2>
+      <h2>React Style</h2>
 
-      <CssModuleComponents />
+      {/* <CssModuleComponents />
 
       <hr />
 
       <SassComponents />
       <hr />
-      <StyledComponent />
+      <StyledComponent /> */}
+
+      {/* <Practice01 /> */}
+      <Practice02 />
     </div>
   );
 }
---
+++ 30-styles/src/Practice02.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/Practice02.scss';
+
+export const Practice02 = () => {
+  return (
+    <div>
+        <div className='wrap'>
+            <div className='ball first'></div>
+            <div className='ball second'></div>
+            <div className='ball third'></div>
+        </div>
+
+    </div>
+  )
+}
