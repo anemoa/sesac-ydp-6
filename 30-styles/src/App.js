fb548aea63d3f6b686d233f44283433d984ae356
anemoa <anemoia.yves@gmail.com>
2024-08-12 17:40:52 +0900

    study: module.css & Sass

---
+++ 30-styles/src/App.js
@@ -0,0 +1,19 @@
+import './styles/App.css';
+import {CssModuleComponents} from './CssModuleComponents';
+import { SassComponents } from './SassComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hihi</h2>
+
+      <CssModuleComponents />
+
+      <hr />
+
+      <SassComponents />
+    </div>
+  );
+}
+
+export default App;
